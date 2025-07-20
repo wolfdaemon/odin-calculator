029daf530679f4fad2579364189dd76e1953ec21
Michael "Cal" Krug <wolfdaemon@murena.io>
2025-07-19 23:15:24 -0500

    feat: calculator styling, event listening, func
    Remove redundant buttons on calculator, add styling to make certain
    buttons/numpad to fit within the dimensional constrains of the
    calculator, add functions to add, subtract, multiply, divide, and
    "operate" (i.e., take two numbers & operate with one of the previous
    functions).

---
+++ index.js
@@ -0,0 +1,69 @@
+function add(num, num1) {
+	return num + num1;
+};
+
+function subtract(num, num1) {
+	return num - num1;
+};
+
+function multiply(num, num1) {
+	return num * num1;
+};
+
+function divide(num, num1) {
+	return num / num1;
+}
+
+function operate(num, num1, btn) {
+	switch (btn) {
+		case "addi":
+			return add(num, num1);
+			break;
+		case "subt":
+			return subtract(num, num1);
+			break;
+		case "mult":
+			return multiply(num, num1);
+			break;
+		case "divi":
+			return divide(num, num1);
+			break;
+	}
+}
+
+const calculator = document.querySelector(".calculator");
+
+calculator.addEventListener("click", (e) => {
+	console.log(e.target.innerHTML);
+});
+
+/*
+function sum(arr) {
+	return arr.reduce((accumulator, element) => { return accumulator + element }, 0);
+};
+*/
+
+/*
+function multiplyArray(arr) {
+	return arr.reduce((acc, item) => { return acc * item });
+};
+*/
+
+/*
+const power = function(num, num1) {
+	let arr = [];
+	for (i = 0; i < num1; i++) {
+		arr.push(num);
+	}
+	return arr.reduce((acc, item) => { return acc * item });
+};
+
+const factorial = function(num) {
+	let arr = [];
+	num = num + 1;
+	for (i = 1; i < num; i++) {
+		arr.push(i);
+	}
+	return arr.reduce((acc, item) => { return acc * item }, 1);
+};
+*/
